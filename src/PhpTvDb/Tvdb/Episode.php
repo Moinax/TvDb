95a754d01f6967e44d58f12ad064f50e12b1d703
Jerome Poskin <jerome@snowcap.be>
2011-10-26 01:15:10 +0200

    Refactoring structure

---
+++ src/PhpTvDb/Tvdb/Episode.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace PhpTvDb\TvDb;
+
+use PhpTvDb\Tvdb\Tvdb;
+
+/**
+ * TV_Episode class. Class for single tv episode for a TV show.
+ *
+ * @package PhpTvDb
+ * @author Ryan Doherty <ryan@ryandoherty.com>
+ * @author Jérôme Poskin <moinax@gmail.com>
+ **/
+class Episode extends Tvdb
+{
+
+    /**
+     * The tvdb.com episode id
+     *
+     * @access public
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * The season number
+     *
+     * @access public
+     * @var integer|string
+     */
+    public $season;
+
+    /**
+     * The episode number for the season
+     *
+     * @access public
+     * @var integer|string
+     */
+    public $number;
+
+    /**
+     * The episode name
+     *
+     * @access public
+     * @var string
+     */
+    public $name;
+
+    /**
+     * First air date of the episode measured in number of seconds from the epoch
+     *
+     * @access public
+     * @var int
+     */
+    public $firstAired;
+
+    /**
+     * Array of guest star names (strings)
+     *
+     * @access public
+     * @var array
+     */
+    public $guestStars;
+
+    /**
+     * Array of director names (strings)
+     *
+     * @access public
+     * @var array
+     */
+    public $directors;
+
+    /**
+     * Array of writers names (strings)
+     *
+     * @access public
+     * @var array
+     */
+    public $writers;
+
+    /**
+     * Overview of the episode
+     *
+     * @access public
+     * @var string
+     */
+    public $overview;
+
+    /**
+     * IMDB id (http://imdb.com/title/$imdbId)
+     *
+     * @access public
+     * @var string
+     */
+    public $imdbId;
+
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return void
+     * @param simplexmlobject $config simplexmlobject created from thetvdb.com's xml data for the tv episode
+     **/
+    function __construct($config)
+    {
+
+
+        $this->id = (string)$config->id;
+        $this->season = (string)$config->SeasonNumber;
+        $this->number = (string)$config->EpisodeNumber;
+        $this->episode = (string)$config->EpisodeNumber;
+        $this->firstAired = strtotime((string)$config->FirstAired);
+        $this->guestStars = $this->removeEmptyIndexes(explode('|', (string)$config->GuestStars));
+        $this->guestStars = array_map('trim', $this->guestStars);
+        $this->directors = $this->removeEmptyIndexes(explode('|', (string)$config->Director));
+        $this->writers = $this->removeEmptyIndexes(explode('|', (string)$config->Writer));
+        $this->overview = (string)$config->Overview;
+        $this->imdbId = (string)$config->IMDB_ID;
+        $this->name = (string)$config->EpisodeName;
+    }
+}
+
+?>
